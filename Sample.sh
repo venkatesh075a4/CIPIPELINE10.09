d1324aec5c4b4d0ae8419be0dcdea2196a2afa01
venkatesh075a4 <venkatesh075a4@gmail.com>
2019-09-10 15:06:43 +0530

    Create Sample.sh

---
+++ Sample.sh
@@ -0,0 +1,11 @@
+if [ $# -gt 0 ]
+then
+        if [ -f $1 ]
+        then
+                cat $1
+        else
+                echo "File not found"$1
+        fi
+else
+        echo "Not enough command line"
+fi
